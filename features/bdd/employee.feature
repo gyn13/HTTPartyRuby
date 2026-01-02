471b6286ea54448e50e76df63f4249184140b783
Yago Barbosa <126727413+gyn13@users.noreply.github.com>
2026-01-02 19:13:27 -0300

    add bdd e step_definitions para post

--- features/bdd/employee.feature
+++ features/bdd/employee.feature
@@ -9,3 +9,9 @@ Cenario: Buscar informacoes de funcionario
 Dado que o usuario consulte informacoes de funcionario
 Quando ele realizar a pesquisa 
 Entao uma lista de funcionarios deve retornar
+
+@cenario_dois
+Cenario: Cadastrar funcionario
+Dado que o usuario cadastre um novo funcionario
+Quando ele enviar as informacoes do funcionario
+Entao esse funcionario sera cadastrado
---
+++ features/step_definitions/employee.rb
@@ -0,0 +1,39 @@
+Dado('que o usuario consulte informacoes de funcionario') do
+  @get_url = 'http://dummy.restapiexample.com/api/v1/employees'
+end
+
+Quando('ele realizar a pesquisa') do
+  @list_employee = HTTParty.get(@get_url)
+end
+
+Entao('uma lista de funcionarios deve retornar') do
+  expect(@list_employee.code).to eql 200
+  expect(@list_employee.message).to eql 'OK'
+end
+
+
+Dado('que o usuario cadastre um novo funcionario') do
+  @post_url = 'http://dummy.restapiexample.com/api/v1/create'
+end
+
+Quando('ele enviar as informacoes do funcionario') do
+  @create_employee = HTTParty.post(@post_url, :headers => {'Content-Type': 'application/json'},body: {
+    "id": 27,
+    "employee_name": "Tony",
+    "employee_salary": 420800,
+    "employee_age": 30,
+    "profile_image": ""
+}.to_json)
+
+puts @create_employee
+end
+
+Entao('esse funcionario sera cadastrado') do
+  expect(@create_employee.code).to eql (200)
+  expect(@create_employee.msg).to eql 'OK' 
+  expect(@create_employee["status"]).to eql 'success'
+  expect(@create_employee["message"]).to eql 'Successfully! Record has been added.'
+  expect(@create_employee.parsed_response['data']["employee_name"]).to eql 'Tony'
+  expect(@create_employee.parsed_response['data']["employee_salary"]).to eql (420800)
+  expect(@create_employee.parsed_response['data']["employee_age"]).to eql (30)
+end
